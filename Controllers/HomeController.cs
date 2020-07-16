ca483cf529af921892e56f86536ac138714522b7
aquagrrl <erin.aka.maggie@gmail.com>
2020-07-16 16:35:49 +0200

    asdasda

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using programmersGuide.Models.DTOs;
+using programmersGuide.Services.Interfaces;
 
 namespace programmersGuide.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IReviewService reviewService;
+
+        public HomeController(IReviewService reviewService)
+        {
+            this.reviewService = reviewService;
+        }
+
         public IActionResult Index()
         {
+            //var model = reviewService.LoadAllReviews();
             return View();
+        }
+
+        [HttpPost("Review")]
+        public async Task<IActionResult> SaveReview(ReviewDTO reviewDTO)
+        {
+            await reviewService.SaveReview(reviewDTO);
+            return Redirect("Index");
         }
     }
 }
