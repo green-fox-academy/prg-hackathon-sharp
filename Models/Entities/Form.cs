ee7d83b0f4699fdf5620366d600deeac691c230a
aquagrrl <erin.aka.maggie@gmail.com>
2020-07-16 12:16:51 +0200

    Form entity completed

--- Models/Entities/Form.cs
+++ Models/Entities/Form.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace programmersGuide.Models
@@ -10,6 +11,19 @@ namespace programmersGuide.Models
         public long ID { get; set; }
         public DateTime Time {get; set;}
         public string Name { get; set; }
-        public string Role { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; }
+        public Int16 Rating { get; set; }
+        public string Review { get; set; }
+    }
+
+    public enum Role
+    {
+        [EnumMember(Value = "frontend")]
+        frontend,
+        [EnumMember(Value = "backend")]
+        backend,
+        [EnumMember(Value = "fullstack")]
+        fullstack
     }
 }
