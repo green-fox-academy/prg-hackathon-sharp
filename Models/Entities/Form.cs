04911ad38755575704d8a081241c73beaff837f9
aquagrrl <erin.aka.maggie@gmail.com>
2020-07-16 12:12:11 +0200

    Form model created

---
+++ Models/Entities/Form.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace programmersGuide.Models
+{
+    public class Form
+    {
+        public long ID { get; set; }
+        public DateTime Time {get; set;}
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
