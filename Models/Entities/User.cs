99e874c7c089bcddc49e9a46a9b9c9c69b310af5
aquagrrl <erin.aka.maggie@gmail.com>
2020-07-17 11:07:30 +0200

    Commiting before pulling

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -9,6 +9,7 @@ namespace programmersGuide.Models.Entities
     {
         public long ID { get; set; }
         public string Username { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         public QuizResult QuizResult { get; set; }
     }
